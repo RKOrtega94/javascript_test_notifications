08a6810b74ff108285833a1aaf710638da9d7657
RKOrtega94 <rkortega1994@gmail.com>
2024-01-18 17:00:20 -0500

    Add config example

---
+++ assets/js/config.example.js
@@ -0,0 +1,22 @@
+/**
+ * This file is an example of the config.js file that is used by the application.
+ *
+ * Copy this file to config.js and change the values to match your environment.
+ *
+ *
+ * @instructions
+ * 1. Copy this file to config.js
+ * 2. Go to the Firebase Console and create a new project (https://console.firebase.google.com/) or open an existing one.
+ * 3. Add a new web app to the project (click the "</>" icon) and give it a name.
+ * 4. Go to "Project Settings" and copy the "firebaseConfig" object.
+ */
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
+};
